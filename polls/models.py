52fdb0238becaa32d8280edce0b93f58d47c3201
quirozc <equiroz@ciencias.unam.mx>
2018-11-04 23:09:05 -0600

    Añadiendo un método para la representación en cadena de las instancias

--- polls/models.py
+++ polls/models.py
@@ -8,38 +8,63 @@ class Person(models.Model):
     mail = models.EmailField(max_length = 200)
     phone_number = models.TextField(max_length = 200)
 
+    def __str__(self):
+        return self.name
+
 class Institution(models.Model):
     id_institution = models.AutoField(primary_key = True)
     name = models.TextField(max_length = 200)
 
+    def __str__(self):
+        return self.name
+
 class Branch(models.Model):
     id_branch = models.AutoField(primary_key = True)
-    institution = models.ForeignKey(Institution, on_delete = models.CASCADE)
+    institution = models.ForeignKey(Institution, related_name = 'branches',  on_delete = models.CASCADE)
     name = models.TextField(max_length = 200)
+
+    def __str__(self):
+        return 'branch: ' + self.name
 
 class Department(models.Model):
     id_department = models.AutoField(primary_key = True)
+    name = models.TextField(max_length = 200)
     phone_number = models.TextField(max_length = 200)
     adress = models.TextField(max_length = 200)
-    branch = models.ForeignKey(Branch, on_delete = models.CASCADE)
+    branch = models.ForeignKey(Branch, related_name = 'departments', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return 'department of: ' + self.name
 
 class Researcher(models.Model):
     id_researcher = models.AutoField(primary_key = True)
-    person = models.ForeignKey(Person, on_delete = models.CASCADE)
-    department = models.ForeignKey(Department, on_delete = models.CASCADE)
+    person = models.ForeignKey(Person, related_name = 'researcher', on_delete = models.CASCADE)
+    department = models.ForeignKey(Department, related_name = 'researchers', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return 'researcher ' + self.person.name
 
 class Student(models.Model):
     id_student = models.AutoField(primary_key = True)
-    person = models.ForeignKey(Person, on_delete = models.CASCADE)
-    supervisor = models.ForeignKey(Researcher, on_delete = models.CASCADE)
+    person = models.ForeignKey(Person, related_name = 'student', on_delete = models.CASCADE)
+    supervisor = models.ForeignKey(Researcher, related_name = 'students', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return 'student ' + self.name
 
 class Group(models.Model):
     id_group = models.AutoField(primary_key = True)
     name = models.TextField(max_length = 200)
-    members = models.ManyToManyField(Person)
-    leader = models.ForeignKey(Researcher, on_delete = models.CASCADE)
+    members = models.ManyToManyField(Person, related_name = 'groups')
+    leader = models.ForeignKey(Researcher, related_name = 'leader', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     id_article = models.AutoField(primary_key = True)
     title = models.TextField(max_length = 200)
-    authors = models.ManyToManyField(Person)
+    authors = models.ManyToManyField(Person, related_name = 'articles')
+
+    def __str__(self):
+        return self.title
