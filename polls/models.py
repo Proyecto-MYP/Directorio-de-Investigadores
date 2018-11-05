8d713c2a0bfe65d61a07bb994c6e165b9fe7daa4
quirozc <equiroz@ciencias.unam.mx>
2018-11-04 19:51:25 -0600

    Cambiando el nombre y tipo de algunos campos.
    Se quitó el prefijo "id" de todas las llaves foráneas. Se cambio lo que teníamos pensado como listas de personas a una tabla de relaciones de muchos a muchos.

--- polls/models.py
+++ polls/models.py
@@ -12,36 +12,34 @@ class Institution(models.Model):
     id_institution = models.AutoField(primary_key = True)
     name = models.TextField(max_length = 200)
 
+class Branch(models.Model):
+    id_branch = models.AutoField(primary_key = True)
+    institution = models.ForeignKey(Institution, on_delete = models.CASCADE)
+    name = models.TextField(max_length = 200)
+
+class Department(models.Model):
+    id_department = models.AutoField(primary_key = True)
+    phone_number = models.TextField(max_length = 200)
+    adress = models.TextField(max_length = 200)
+    branch = models.ForeignKey(Branch, on_delete = models.CASCADE)
+
+class Researcher(models.Model):
+    id_researcher = models.AutoField(primary_key = True)
+    person = models.ForeignKey(Person, on_delete = models.CASCADE)
+    department = models.ForeignKey(Department, on_delete = models.CASCADE)
+
+class Student(models.Model):
+    id_student = models.AutoField(primary_key = True)
+    person = models.ForeignKey(Person, on_delete = models.CASCADE)
+    supervisor = models.ForeignKey(Researcher, on_delete = models.CASCADE)
+
 class Group(models.Model):
     id_group = models.AutoField(primary_key = True)
     name = models.TextField(max_length = 200)
-    members = models.TextField(max_length = 200)
-    leader = models.ForeignKey(Person, on_delete = models.CASCADE)
-
-class Branch(models.Model):
-    id_branch = models.AutoField(primary_key = True)
-    id_institution = models.ForeignKey(Institution, on_delete = models.CASCADE)
-    name = models.TextField(max_length = 200)
+    members = models.ManyToManyField(Person)
+    leader = models.ForeignKey(Researcher, on_delete = models.CASCADE)
 
 class Article(models.Model):
     id_article = models.AutoField(primary_key = True)
     title = models.TextField(max_length = 200)
-    authors = models.TextField(max_length = 200)
-    
-class Department(models.Model):
-    id_department = models.AutoField(primary_key = True)
-    phone_number = models.TextField(max_length = 200)
-    direction = models.TextField(max_length = 200)
-    id_branch = models.ForeignKey(Branch, on_delete = models.CASCADE)
-    
-class Investigator(models.Model):
-    id_investigator = models.AutoField(primary_key = True)
-    id_person = models.ForeignKey(Person, on_delete = models.CASCADE)
-    id_department = models.ForeignKey(Department, on_delete = models.CASCADE)
-
-class Student(models.Model):
-    id_student = models.AutoField(primary_key = True)
-    id_person = models.ForeignKey(Person, on_delete = models.CASCADE)
-    id_investigator = models.ForeignKey(Investigator, on_delete = models.CASCADE)
-    
-    
+    authors = models.ManyToManyField(Person)
