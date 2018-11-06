50a7b6059160dec2ae2360fe4a4868af56b34a6b
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2018-11-06 12:55:10 -0600

    Se pasó la lista de estados a un servicio

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from './map.service';
 
 @Component({
   selector: 'app-map',
@@ -7,63 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  estados = this.mapSrv.getEstados();
+
+  constructor( private mapSrv: MapService) { }
 
   ngOnInit() {
     const $mapa = document.getElementsByTagName('svg')[0]
     const $etiqueta = document.getElementById('etiqueta');
     const $estadosSelect = document.getElementById('estadoSelect') as HTMLSelectElement;
-    const estados = {
-        "AS": "Aguascalientes",
-        "BC": "Baja California",
-        "BS": "Baja California Sur",
-        "CC": "Campeche",
-        "CS": "Chiapas",
-        "CH": "Chihuahua",
-        "CL": "Coahuila",
-        "CM": "Colima",
-        "DF": "Ciudad de México",
-        "DG": "Durango",
-        "GT": "Guanajuato",
-        "GR": "Guerrero",
-        "HG": "Hidalgo",
-        "JC": "Jalisco",
-        "MC": "Estado de México",
-        "MN": "Michoacán",
-        "MS": "Morelos",
-        "NT": "Nayarit",
-        "NL": "Nuevo León",
-        "OC": "Oaxaca",
-        "PL": "Puebla",
-        "QO": "Querétaro",
-        "QR": "Quintana Roo",
-        "SP": "San Luis Potosí",
-        "SL": "Sinaloa",
-        "SR": "Sonora",
-        "TC": "Tabasco",
-        "TS": "Tamaulipas",
-        "TL": "Tlaxcala",
-        "VZ": "Veracruz",
-        "YN": "Yucatán",
-        "ZS": "Zacatecas"
-    }
+    const estadosHash = this.mapSrv.getHash();
 
-    // Poblar el select con los estados
-    for(let key in estados){
-      let $option = document.createElement('option');
-      $option.value = key;
-      $option.text = estados[key];
-      $estadosSelect.add($option, null);
-    }
-    
     // Mostrar el estado en la etiqueta
     $mapa.addEventListener('mouseover', function(evt){
       var target = evt.target as HTMLElement;
       var key = target.dataset.key;
-      var edo = estados[key];
+      var edo = estadosHash[key];
       $etiqueta.innerText = edo ? edo : '';
     });
-    
+
     // Enviar formulario al hacer clic en un edo
     $mapa.addEventListener('click', function(evt){
       var $formulario = document.getElementById('mapaForm') as HTMLFormElement;
@@ -74,8 +36,10 @@ export class MapComponent implements OnInit {
         $estadosSelect.value = key;
         $formulario.submit();
       }
-      
+
     });
 
   }
+
+
 }
---
+++ src/app/map/map.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  estados = [
+    { clave: 'AS', nombre: "Aguascalientes" },
+    { clave: 'BC', nombre: "Baja California" },
+    { clave: 'BS', nombre: "Baja California Sur" },
+    { clave: 'CC', nombre: "Campeche" },
+    { clave: 'CS', nombre: "Chiapas" },
+    { clave: 'CH', nombre: "Chihuahua" },
+    { clave: 'CL', nombre: "Coahuila" },
+    { clave: 'CM', nombre: "Colima" },
+    { clave: 'DF', nombre: "Ciudad de México" },
+    { clave: 'DG', nombre: "Durango" },
+    { clave: 'GT', nombre: "Guanajuato" },
+    { clave: 'GR', nombre: "Guerrero" },
+    { clave: 'HG', nombre: "Hidalgo" },
+    { clave: 'JC', nombre: "Jalisco" },
+    { clave: 'MC', nombre: "Estado de México" },
+    { clave: 'MN', nombre: "Michoacán" },
+    { clave: 'MS', nombre: "Morelos" },
+    { clave: 'NT', nombre: "Nayarit" },
+    { clave: 'NL', nombre: "Nuevo León" },
+    { clave: 'OC', nombre: "Oaxaca" },
+    { clave: 'PL', nombre: "Puebla" },
+    { clave: 'QO', nombre: "Querétaro" },
+    { clave: 'QR', nombre: "Quintana Roo" },
+    { clave: 'SP', nombre: "San Luis Potosí" },
+    { clave: 'SL', nombre: "Sinaloa" },
+    { clave: 'SR', nombre: "Sonora" },
+    { clave: 'TC', nombre: "Tabasco" },
+    { clave: 'TS', nombre: "Tamaulipas" },
+    { clave: 'TL', nombre: "Tlaxcala" },
+    { clave: 'VZ', nombre: "Veracruz" },
+    { clave: 'YN', nombre: "Yucatán" },
+    { clave: 'ZS', nombre: "Zacatecas" },
+  ];
+
+  constructor() { }
+
+  getEstados(){
+    return this.estados;
+  }
+
+  getHash(){
+    let hash = {};
+    for(let edo of this.estados){
+      hash[edo.clave] = edo.nombre
+    }
+    return hash;
+  }
+
+}
