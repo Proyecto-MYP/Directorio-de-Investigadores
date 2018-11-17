e38bc6e596186da353ac99020b8aa8be6d04db8c
Mauricio <mauriciochavez@ciencias.unam.mx>
2018-11-16 23:09:53 -0600

    Añadido: :star: Vista de registro con una primer versión del registro de usuarios :star: Fuente Nunito de la API de Google Fonts para todo el sitio :star: Animación del botón de registro y de inicio de sesión
    Arreglado:
    :rocket: Bug que hacía que el login en una ventana mediana se viera a la mitad
    
    Cambios:
    :octocat: En el login, en el input del correo, el tipo (de text a email)

--- views/src/app/pages/usuario/registro/registro.component.ts
+++ views/src/app/pages/usuario/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title, 
+    private usuarioService: UsuarioService
+  ) {}
 
   ngOnInit() {
+    this.titleService.setTitle('Regístrate');
+  }
+
+  registerUser(){
+  	const username = document.getElementById('username') as HTMLInputElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+    const password_confirmation = document.getElementById('password_confirmation') as HTMLInputElement;
+    
+    this.usuarioService.registerUser(username.value, email.value, password.value, password_confirmation.value);
   }
 
 }
