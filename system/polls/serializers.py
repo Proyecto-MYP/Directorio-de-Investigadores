45def53d36526609004780da0d59760681dee60f
Edgar Quiroz <equiroz@ciencias.unam.mx>
2018-11-20 17:12:32 -0600

    Añadiendo busquedas

--- system/polls/serializers.py
+++ system/polls/serializers.py
@@ -14,27 +14,30 @@ class PersonSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = models.Person
-        fields = ('id_person', 'name', 'birthdate', 'mail', 'phone_number', 'articles', 'groups')
+        fields = (
+		'id_person', 'name', 'birthdate', 'mail', 'phone_number',
+		'articles', 'groups', 'created_at'
+		)
 
 class InstitutionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Institution
-        fields = ('id_institution', 'name')
+        fields = ('id_institution', 'name', 'created_at')
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Group
-        fields = ('id_group', 'name', 'members', 'leader')
+        fields = ('id_group', 'name', 'members', 'leader', 'created_at')
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Branch
-        fields = ('id_branch', 'institution', 'name', 'state')
+        fields = ('id_branch', 'institution', 'name', 'state', 'created_at')
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Article
-        fields = ('id_article', 'title', 'authors')
+        fields = ('id_article', 'title', 'authors', 'created_at')
 
 class DepartmentSerializer(serializers.ModelSerializer):
     researchers = serializers.HyperlinkedRelatedField(
@@ -44,7 +47,8 @@ class DepartmentSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = models.Department
-        fields = ('id_department', 'name', 'phone_number', 'adress', 'branch', 'researchers')
+        fields = ('id_department', 'name', 'phone_number', 'adress', 'branch',
+		'researchers', 'created_at')
 
 class ResearcherSerializer(serializers.ModelSerializer):
     students = serializers.HyperlinkedRelatedField(
@@ -59,14 +63,15 @@ class ResearcherSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = models.Researcher
-        fields = ('id_researcher', 'person', 'department', 'students', 'leader')
+        fields = ('id_researcher', 'person', 'department', 'students', 'leader',
+		'created_at')
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Student
-        fields = ('id_student', 'person', 'supervisor')
+        fields = ('id_student', 'person', 'supervisor', 'created_at')
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.States
-        fields = ('id_state','name', 'slug')
+        fields = ('id_state','name', 'slug', 'created_at')
--- system/polls/views.py
+++ system/polls/views.py
@@ -5,35 +5,45 @@ from . import serializers
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = models.Person.objects.all()
     serializer_class = serializers.PersonSerializer
+    filter_fields = ('id_person', 'name', 'birthdate', 'mail', 'phone_number', 'articles', 'groups')
 
 class InstitutionViewSet(viewsets.ModelViewSet):
     queryset = models.Institution.objects.all()
     serializer_class = serializers.InstitutionSerializer
+    filter_fields = ('id_institution', 'name')
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = models.Group.objects.all()
     serializer_class = serializers.GroupSerializer
+    filter_fields = ('id_group', 'name', 'members', 'leader')
 
 class BranchViewSet(viewsets.ModelViewSet):
     queryset = models.Branch.objects.all()
     serializer_class = serializers.BranchSerializer
+    filter_fields = ('id_branch', 'institution', 'name', 'state')
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = models.Article.objects.all()
     serializer_class = serializers.ArticleSerializer
+    filter_fields = ('id_article', 'title', 'authors')
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = models.Department.objects.all()
     serializer_class = serializers.DepartmentSerializer
+    filter_fields = ('id_department', 'name', 'phone_number', 'adress', 'branch', 'researchers')
 
 class ResearcherViewSet(viewsets.ModelViewSet):
     queryset = models.Researcher.objects.all()
     serializer_class = serializers.ResearcherSerializer
+    filter_fields = ('id_researcher', 'person', 'department', 'students', 'leader')
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = models.Student.objects.all()
     serializer_class = serializers.StudentSerializer
+    filter_fields = ('id_student', 'person', 'supervisor')
 
 class StateViewSet(viewsets.ModelViewSet):
     queryset = models.States.objects.all()
     serializer_class = serializers.StateSerializer
+    filter_fields = ('id_state','name', 'slug')
