7e72eb733d58f092ee9f71567541ed6dc731e606
Ricardo <ricardo_glz@ciencias.unam.mx>
2018-11-25 20:15:21 -0600

    Corre con nginx

--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-#exec python system/manage.py makemigrations polls
-#exec python system/manage.py makemigrations users
-#exec python system/manage.py migrate
+python system/manage.py makemigrations polls
+python system/manage.py makemigrations users
+python system/manage.py migrate
 exec gunicorn Backend.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
     --chdir system/
-
