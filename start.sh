aadeadb790718e67342883dc04f228b8a84a1cb3
Ricardo <ricardo_glz@ciencias.unam.mx>
2018-12-11 20:57:21 -0600

    Errores chiquitos corregidos

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-python system/manage.py makemigrations polls
-python system/manage.py makemigrations users
-python system/manage.py migrate
 exec gunicorn Backend.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
