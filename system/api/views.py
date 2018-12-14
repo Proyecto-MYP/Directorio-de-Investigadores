6318de3d1124e526a45c55a5fe9efc33d18d1e64
Edgar Quiroz <equiroz@ciencias.unam.mx>
2018-12-13 23:42:38 -0600

    Vista especial para sobreescribir la vista por omisión que no hace nada

--- system/api/views.py
+++ system/api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_auth.registration.serializers import VerifyEmailSerializer
+from allauth.account.models import EmailConfirmation, EmailConfirmationHMAC
 
-# Create your views here.
+class ConfirmEmailView(APIView):
+    permission_classes = [AllowAny]
+
+    def get_serializer(self, *args, **kwargs):
+        return VerifyEmailSerializer(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        print(request.data)
+        serializer = self.get_serializer(data=kwargs)
+        serializer.is_valid(raise_exception=True)
+        self.kwargs['key'] = serializer.validated_data['key']
+        self.object = confirmation = self.get_object()
+        confirmation.confirm(self.request)
+        return Response({'detail': _('ok')}, status=status.HTTP_200_OK)
+
+    def get_object(self, queryset=None):
+        key = self.kwargs['key']
+        email_confirmation = EmailConfirmationHMAC.from_key(key)
+        if not email_confirmation:
+            if queryset is None:
+                queryset = self.get_queryset()
+            try:
+                email_confirmation = queryset.get(key=key.lower())
+            except EmailConfirmation.DoesNotExist:
+                raise Http404()
+        return email_confirmation
+
+    def get_queryset(self):
+        qs = EmailConfirmation.objects.all_valid()
+        qs = qs.select_related("email_address__user")
+        return qs
